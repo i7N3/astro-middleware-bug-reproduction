6aa8fdd4667f516337b5f90ec2d5641c5438cc3d
Nick K <24440824+i7N3@users.noreply.github.com>
2023-09-23 12:48:07 +0400

    Update Astro to fix the bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,8 @@ import { defineMiddleware, sequence } from 'astro/middleware';
 
 export const middleware = defineMiddleware((context, next) => {
     if (context.url.pathname.includes('/login')) {
-        context.cookies.set('jid', '<TOKEN>');
+        context.cookies.set('acc', 'acc');
+        context.cookies.set('jid', 'jid');
         return context.redirect('/dashboard');
     }
     return next();
